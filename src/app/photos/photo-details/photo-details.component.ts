9fbb016b63d769c9eb3debd3126efdbb18063812
Pedro Iunes <pedrohsiunes@gmail.com>
2019-05-20 11:42:38 -0400

    error

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -6,7 +6,6 @@ import { Observable } from 'rxjs';
 
 @Component({
     templateUrl: './photo-details.component.html',
-    styleUrls: ['photo-details.component.css']
 })
 
 export class PhotoDetailsComponent implements OnInit {
--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { VmessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 import { PhotoDetailsComponent } from './photo-details.component';
 import { PhotoModule } from '../photo/photo.module';
 import { PhotoCommentsComponent } from './photo-comments/photo-comments.Component';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     PhotoModule,
     RouterModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VmessageModule
   ],
   exports: [
     PhotoDetailsComponent,
